1fb0a1c20574a1d4c0f40a0ebf6bad4266a7ffc3
worood assi <woroodassi345@gmail.com>
2024-09-11 09:43:41 +0300

    meta data

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,9 +3,11 @@ import About from '@/components/About';
 import SkillsList from '@/components/SkillsList';
 import ProjectsList from '@/components/ProjectsList';
 import Work from '@/components/Work';
+import { Helmet } from 'react-helmet';
+import me1 from '@/assets/me1.png';
 
 const HomePage = () => {
-    document.title = "Personal Website | Worood Assi";
+    // document.title = "Personal Website | Worood Assi";
     const navigate = useNavigate();
 
     const handleButtonClick = () => {
@@ -14,6 +16,23 @@ const HomePage = () => {
     
     return (
         <>
+            <Helmet>
+                {/* Open Graph Meta Tags */}
+                <title>Personal Website | Worood Assi</title>
+                <meta property="og:title" content="Personal Website | Worood Assi" />
+                <meta property='og:description' content='This responsive personal portfolio is built with React, TypeScript, and Tailwind CSS. It effectively showcases my skills, projects, and professional experience.' />
+                <meta property="og:type" content="website" />
+                <meta property="og:url" content="https://woroodassi.netlify.app" />
+                <meta property="og:image" content={me1} />
+                <meta property="og:site_name" content="Worood Assi" />
+
+                {/* Twitter Meta Tags */}
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta name="twitter:title" content="Personal Website | Worood Assi" />
+                <meta name="twitter:description" content="This responsive personal portfolio is built with React, TypeScript, and Tailwind CSS. It effectively showcases my skills, projects, and professional experience." />
+                <meta name="twitter:image" content={me1} />
+                <meta name="twitter:site" content="Worood Assi" />
+            </Helmet>
             <div className='container mx-auto py-7'>
                 <About />
             </div>
